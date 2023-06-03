8da3795247e64f06f57f51b5d8fce5bde9bce7b3
Łukasz Mączka <lukaszmaczka3604@gmail.com>
2023-06-03 14:15:34 +0200

    Removed unnecessary custom Exception

--- src/main/java/dev/monczall/atiperatask/expection/HeaderMediaTypeNotAcceptableException.java
+++
@@ -1,7 +0,0 @@
-package dev.monczall.atiperatask.expection;
-
-public class HeaderMediaTypeNotAcceptableException extends RuntimeException {
-    public HeaderMediaTypeNotAcceptableException(String message){
-        super(message);
-    }
-}
